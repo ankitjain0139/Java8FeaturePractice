61b925a508a8ffcb680fce01985704a9eb7b7638
Ankit Jain <jainankit0139@gmail.com>
2020-07-20 12:29:38 +0530

    added Lombok.

---
+++ Java8FeaturePractice/src/demo/streams/LombokService.java
@@ -0,0 +1,16 @@
+package demo.streams;
+
+public class LombokService {
+	
+	
+	public static void main(String[] args) {
+		LombokTest lombokTest = new LombokTest();
+		
+		lombokTest.setId(1);
+		lombokTest.setName("ankit");
+		
+		System.out.println("id :"+lombokTest.getId() + " & " +" name :"+lombokTest.getName());
+		
+	}
+
+}
---
+++ Java8FeaturePractice/src/demo/streams/LombokTest.java
@@ -0,0 +1,11 @@
+package demo.streams;
+
+import lombok.Data;
+
+@Data
+public class LombokTest {
+	
+	private int id;
+	private String name;
+
+}
